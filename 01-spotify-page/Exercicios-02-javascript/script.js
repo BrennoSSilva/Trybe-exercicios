9665b50938d8467da0631b2e9701b57f181de9d3
Brenno S Silva <brennosoares2@gmail.com>
2025-03-12 20:40:53 -0300

    Resolvendo erro .

--- 01-spotify-page/Exercicios-02-javascript/script.js
+++ 01-spotify-page/Exercicios-02-javascript/script.js
@@ -1,1 +1,13 @@
-console.log('Ola Mundo!');
+let nun1 = 100;
+let nun2 = 100;
+let word = 'Palavra';
+let xablau = null;
+let babalu =undefined;
+
+if (nun1 > nun2){
+    console.log('nun1 é maior');
+} else if (nun1 == nun2) {
+    console.log('Igual');
+} else {
+    console.log('nun2 é maior');
+};
