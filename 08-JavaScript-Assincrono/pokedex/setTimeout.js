61e5668857a3f0fc327190f5e7aa685275a813ab
Brenno S Silva <brennosoares2@gmail.com>
2025-05-28 19:37:18 -0300

    Pokedex

---
+++ 08-JavaScript-Assincrono/pokedex/setTimeout.js
@@ -0,0 +1,16 @@
+
+const funcPromise = async () => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Xablau');
+    }, 5000);
+  })
+
+  await promise.then((response) => {
+    console.log(response);
+  })
+
+  return promise;
+};
+
+funcPromise();
